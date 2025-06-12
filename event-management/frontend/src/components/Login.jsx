a17c30764c6e9178597e832c33ca7becb12a563d
prathamsuroshi <96018698+prathamsuroshi@users.noreply.github.com>
2025-06-12 16:14:03 +0530

    Update Login.jsx

--- event-management/frontend/src/components/Login.jsx
+++ event-management/frontend/src/components/Login.jsx
@@ -11,7 +11,7 @@ const Login = ({ onLoginSuccess }) => {
   const loginHandler = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:10000/api/auth/login', {
+      const res = await axios.post('https://event-management-5-tjjq.onrender.com/api/auth/login', {
         email,
         password,
       });
